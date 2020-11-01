603222e2caf3344892b65e6702f733c4a9c87fca
likinus <59907622+likinus@users.noreply.github.com>
2020-11-01 17:16:44 +0300

    Add files via upload

---
+++ build/precache-manifest.5ab74b49d89fc25ddb4baa40bc6c0ff6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "012fcd0a9c5e5b30aad3eaaa1876475f",
+    "url": "/Social-NetWork/index.html"
+  },
+  {
+    "revision": "a39fa19a90bf9950beb2",
+    "url": "/Social-NetWork/static/css/3.a7595416.chunk.css"
+  },
+  {
+    "revision": "376f274f1a13c223661c",
+    "url": "/Social-NetWork/static/css/4.2c042b2e.chunk.css"
+  },
+  {
+    "revision": "e6eec6e9ce7b98162436",
+    "url": "/Social-NetWork/static/css/main.ebf40200.chunk.css"
+  },
+  {
+    "revision": "293bb3c3efd639b6abed",
+    "url": "/Social-NetWork/static/js/2.1e56bae1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Social-NetWork/static/js/2.1e56bae1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a39fa19a90bf9950beb2",
+    "url": "/Social-NetWork/static/js/3.b6a7c4dc.chunk.js"
+  },
+  {
+    "revision": "376f274f1a13c223661c",
+    "url": "/Social-NetWork/static/js/4.eeae3f10.chunk.js"
+  },
+  {
+    "revision": "e6eec6e9ce7b98162436",
+    "url": "/Social-NetWork/static/js/main.791970df.chunk.js"
+  },
+  {
+    "revision": "06695686bd40ab7594a6",
+    "url": "/Social-NetWork/static/js/runtime-main.23d99a28.js"
+  }
+]);
